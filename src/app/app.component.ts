66c96b3da027fd8bbc4de54466810222f4223640
Chapelle Nathan <nathanchapelle76@gmail.com>
2022-03-28 19:34:37 +0200

    aozugdogqb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, interval, map, Observable, tap, timeout } from 'rxjs';
 import { FaceSnap } from './models/face-snap.model';
 
 @Component({
@@ -6,7 +7,12 @@ import { FaceSnap } from './models/face-snap.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  interval$ !: Observable<number>;
+
   ngOnInit() {
+    this.interval$ = interval(1000).pipe(
+      map(value => value * 10)
+    );
   }
 }
--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Observable, Subject } from 'rxjs';
+import { tap, takeUntil } from 'rxjs/operators';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapServices } from '../services/face-snap.services';
 
@@ -7,13 +9,22 @@ import { FaceSnapServices } from '../services/face-snap.services';
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
 
   constructor(private faceSnapListService: FaceSnapServices) { }
   faceSnaps !: FaceSnap[];
+  private destroy$!: Subject<boolean>;
+
+  interval$ !: Observable<number>;
 
   ngOnInit(): void {
-    this.faceSnaps = this.faceSnapListService.faceSnaps
+    this.faceSnaps = this.faceSnapListService.faceSnaps;
+    this.destroy$ = new Subject<boolean>();
+    interval(1000).pipe(tap(console.log), takeUntil(this.destroy$)).subscribe();
+  }
+
+  ngOnDestroy(): void{
+    this.destroy$.next(true);
   }
 
 }
